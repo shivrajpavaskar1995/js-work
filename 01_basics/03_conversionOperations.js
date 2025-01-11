a1cc4cb2f4e8d71ae61660dfe228b28808899939
shivrajpavaskar1995 <143311102+shivrajpavaskar1995@users.noreply.github.com>
2025-01-11 17:17:57 +0000

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score=55;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof(score));
+
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
